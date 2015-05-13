a5d816d879a1ba6909eeda8817422e014ca18941
ching56 <F74036467@mail.ncku.edu.tw>
2015-05-13 15:58:32 +0800

    rm block

--- main.cpp
+++ main.cpp
@@ -1,56 +1,36 @@
-#include "block.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 #include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <vector>
 #include <cstdlib>
-using namespace std;
-bool block::exist[16]={false};
-int block::value[16]={0};
+#include <QLabel>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // create a scene
-    QGraphicsScene * scene = new QGraphicsScene();
+    MainWindow w;
+    w.show();
 
-    // create an item to add to the scene
-    vector<block*> blo(16);
+    //creat two random tiles when beginning
     srand(time(NULL));
-    int aa = rand()%16;
-    blo.at(aa)=new block();
-    int b;
-    do{b=rand()%16;}while(rand()%16==aa);
+    int rand_num=rand()%16;
+    int rand_num2;
+    do{rand_num2 = rand()%16;}
+    while(rand_num2==rand_num);
+    /*w.block[rand_num]->show();
+    w.block[rand_num2]->show();
+    w.block[rand_num]->setText("2");
+    w.block[rand_num2]->setText("2");*/
 
-    //when start, creat 2 blcok
-    blo.at(b)=new block();
-    scene->addItem(blo.at(aa));
-    scene->addItem(blo.at(b));
-    blo.at(aa)->setFlag(QGraphicsItem::ItemIsFocusable);
-    blo.at(aa)->setPos(25+aa%4*90,aa/4%4*90+200);
-    blo.at(b)->setFlag(QGraphicsItem::ItemIsFocusable);
-    blo.at(b)->setPos(25+b%4*90,b/4%4*90+200);
-    block::exist[aa]=true;
-    block::exist[b]=true;
-    //test focus
-    blo.at(b)->setFocus();
+    w.block[1]->show();
+    w.block[2]->show();
+    w.block[1]->setText("2");
+    w.block[2]->setText("2");
 
- /*   for(int i=0;i<16;i++){
-    blo.push_back(new block());
-    scene->addItem(blo[i]);
+    w.block[3]->show();
+    w.block[6]->show();
+    w.block[3]->setText("2");
+    w.block[6]->setText("2");
 
-    blo[i]->setFlag(QGraphicsItem::ItemIsFocusable);
-    blo[i]->setPos(25+i%4*90,i/4%4*90+200);
-    }
-*/
-    QGraphicsView * view = new QGraphicsView(scene);
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-
-    view->show();
-    view->setWindowTitle("2048");
-    view->setFixedSize(400,600);
-    scene->setSceneRect(0,0,400,600);
 
 
     return a.exec();
